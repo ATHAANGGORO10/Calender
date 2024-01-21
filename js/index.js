d2efbc75557e61937212f1e0af5f2ecdba3822d9
ATHAANGGORO10 <atha.anggoro2019@gmail.com>
2024-01-21 20:07:11 +0700

    Update

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 const daysTag = document.querySelector(".days");
+const notepad = document.getElementById('notepad');
+const removeBtn = document.getElementById('removeBtn');
 const currentDate = document.querySelector(".current-date");
 const prevNextIcon = document.querySelectorAll(".icons span");
 
 let date = new Date();
 let currYear = date.getFullYear();
 let currMonth = date.getMonth();
-
 const months = [
     "January", "February", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"
@@ -20,17 +21,15 @@ const renderCalendar = () => {
     for (let i = firstDayOfMonth; i > 0; i--) {
         liTag += `<li class="inactive">${lastDateOfLastMonth - i + 1}</li>`;
     } for (let i = 1; i <= lastDateOfMonth; i++) {
-        let isToday = i === date.getDate() && currMonth === new Date().getMonth()
-            && currYear === new Date().getFullYear() ? "active" : "";
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? "active" : "";
         liTag += `<li class="${isToday}">${i}</li>`;
     } for (let i = lastDayOfMonth; i < 6; i++) {
         liTag += `<li class="inactive">${i - lastDayOfMonth + 1}</li>`;
     }
     currentDate.innerText = `${months[currMonth]} ${currYear}`;
     daysTag.innerHTML = liTag;
-};
+}; renderCalendar();
 
-renderCalendar();
 prevNextIcon.forEach(icon => {
     icon.addEventListener("click", () => {
         currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
@@ -43,4 +42,42 @@ prevNextIcon.forEach(icon => {
         }
         renderCalendar();
     });
+});
+
+window.onload = () => {
+    const savedContent = localStorage.getItem('notepadContent');
+    if (savedContent) {
+        notepad.value = savedContent;
+    }
+};
+
+notepad.addEventListener('input', () => {
+    localStorage.setItem('notepadContent', notepad.value);
+});
+
+removeBtn.addEventListener('click', () => {
+    if (notepad.value.trim() === '') {
+        Swal.fire('Please Fill in Notepad');
+    } else {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: 'Once deleted, you will not be able to recover this note!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Dalate',
+            cancelButtonText: 'Cancel!',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                localStorage.removeItem('notepadContent');
+                notepad.value = '';
+                Swal.fire("Deleted!", "Your note has been deleted.", "success", {
+                    icon: 'success',
+                });
+            } else {
+                Swal.fire('Your note is safe!');
+            }
+        });
+    }
 });
